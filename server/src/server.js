b0b1162a99280872717ddb96cfd3ec1fb7e0f5da
Sahil Mistry <your-sufiuddinmistry222@gmail.com>
2026-07-02 18:57:53 +0530

    Add environment variable configuration

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
+import dotenv from "dotenv";
 import app from "./app.js";
 
-const PORT = 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
